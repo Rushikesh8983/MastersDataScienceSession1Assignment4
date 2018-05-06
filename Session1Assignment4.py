c666e59af50b1e245f1f2108d5422c6a0ed112c5
Rushikesh D. Dandagwhal <38999792+Rushikesh8983@users.noreply.github.com>
2018-05-06 12:22:32 +0530

    Add files via upload

---
+++ Session1Assignment4.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#Code to find volume of the sphere
+import math
+pi = math.pi
+D = 12 #D is the diameter of the sphere in 'cm' unit
+r = D/2
+Vsphere = (4/3)*pi*pow (r,3)
+print (Vsphere)
+
